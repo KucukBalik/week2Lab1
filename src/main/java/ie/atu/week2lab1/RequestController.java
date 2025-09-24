7461326eed6ac6aadc335ffb004c2243d4ea6bd4
DERVISEMREDEMIR <g00427391@atu.ie>
2025-09-24 15:08:17 +0100

    Path variable created. User is greeted as their name.

--- src/main/java/ie/atu/week2lab1/RequestController.java
+++ src/main/java/ie/atu/week2lab1/RequestController.java
@@ -1,6 +1,7 @@
 package ie.atu.week2lab1;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,10 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class RequestController {
 
-    @GetMapping
+    @GetMapping("/hello")
     public String hello(){
 
         return "hello";
+    }
+
+    @GetMapping("/greet/{name}")
+    public String greetByName(@PathVariable String name){
+
+        return "Hello " + name + "!";
     }
 
 }
